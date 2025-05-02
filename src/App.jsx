aa9f8ac78c3d1be8cf3a4918b401802d06e41d51
Ineffable-Rock <krishkr8691@gmail.com>
2025-05-02 23:59:33 +0530

    path name correct in appjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css';
-import Navbar from './Components/Navbar/Navbar';
-import AboutMe from './Components/AboutMe/AboutMe';
-import Hero from './components/Hero/Hero';
+import AboutMe from './components/AboutMe/AboutMe.jsx';
+import Hero from './components/Hero/Hero.jsx';
 
-import MyService from './components/MyServices/MyService';
-import MyWork from './Components/MyWork/MyWork';
-import Contact from './Components/Contactform/ContactForm';
+import MyService from './components/MyServices/MyService.jsx';
+import MyWork from './components/MyWork/MyWork.jsx';
+import Contact from './components/Contactform/ContactForm.jsx';
 
-import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
   return (
